5c321e4b3ebed0558d0fab346094697b166375b3
prydegeh <91499002+prydegeh@users.noreply.github.com>
2021-10-19 13:45:04 -0400

    Add files via upload
    Bean Classes Created
    -- working on starter class

---
+++ ExpenseReimbursementSystem/src/com/revature/beans/ReimbursementStatus.java
@@ -0,0 +1,29 @@
+package com.revature.beans;
+
+public class ReimbursementStatus {
+    private int id;
+    private String status;
+    
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public ReimbursementStatus(int id, String status) {
+		super();
+		this.id = id;
+		this.status = status;
+	}
+    
+}
